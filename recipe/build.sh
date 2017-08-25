f0801495b307d31fc74e568109957e3bd7c37ea2
Ray Donnelly <mingw.android@gmail.com>
2017-08-25 13:10:11 +0100

    Add cross-compilation support

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,9 @@
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 
 ./configure --prefix=$PREFIX \
+            --host=${HOST} \
             --with-gmp=$PREFIX \
             --enable-static
-make
+make -j${CPU_COUNT}
 make check
 make install
