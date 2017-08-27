32575d1a4d644be1b53f41615f96ccc1850caa58
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 01:37:16 +0100

    Generalize logging

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
             --host=${HOST} \
             --with-gmp=$PREFIX \
             --enable-static
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
